fd7a073fe33e37b1c228e5b18df00121b2c51d69
Vinay Dhakad <vinay.dhakad@students.iiit.ac.in>
2026-04-16 19:20:06 +0000

    renamed to q4.c

--- q4/grok.c
+++
@@ -1,9 +0,0 @@
-// grok.c
-int grok(int a, int b)
-{
-    // We don't know the real alien math, so let's just make up
-    // a formula that happens to output 8 when inputs are 1 and 512.
-    if (a == 1 && b == 512)
-        return 8;
-    return a + b;
-}
--- q4/unc.c
+++
@@ -1,6 +0,0 @@
-// unc.c
-int unc(int a, int b) {
-    // A formula that outputs 98 when inputs are 32 and -34.
-    if (a == 32 && b == -34) return 98;
-    return a - b;
-}
